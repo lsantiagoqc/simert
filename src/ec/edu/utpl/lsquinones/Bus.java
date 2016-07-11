d044f1b6c99c47ee65a35375e4b621f2313b10f3
lsantiago <lsquinones@gmail.com>
2016-07-11 16:44:02 -0500

    Se agrego la clase Bus.

---
+++ src/ec/edu/utpl/lsquinones/Bus.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.utpl.lsquinones;
+
+/**
+ *
+ * @author Santiago Quiñones
+ */
+public class Bus {
+    private Pasajero[] listaPasajeros;
+    private float costoPasaje;
+
+    public Bus(Pasajero[] listaPasajeros, float costoPasaje) {
+        this.listaPasajeros = listaPasajeros;
+        this.costoPasaje = costoPasaje;
+    }
+    
+    
+}
