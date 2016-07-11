bce702afd0311c44ea381d16c77dbcb8a44f10c0
lsantiago <lsquinones@gmail.com>
2016-07-11 16:56:28 -0500

    Se agregaron propiedades al bus.

--- src/ec/edu/utpl/lsquinones/Bus.java
+++ src/ec/edu/utpl/lsquinones/Bus.java
@@ -14,10 +14,16 @@ public class Bus {
     private Pasajero[] listaPasajeros;
     private float costoPasaje;
 
-    public Bus(Pasajero[] listaPasajeros, float costoPasaje) {
-        this.listaPasajeros = listaPasajeros;
-        this.costoPasaje = costoPasaje;
+    public Bus() {
+        this.listaPasajeros = new Pasajero[10];
+        this.costoPasaje = 0.3F;
     }
+
+    public float getCostoPasaje() {
+        return costoPasaje;
+    }
+    
+    
     
     
 }
