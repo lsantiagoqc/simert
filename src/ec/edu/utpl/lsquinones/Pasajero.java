3fa334f094f3b5e5a582d9446e2b6bccea344d0e
lsantiago <lsquinones@gmail.com>
2016-07-11 14:07:15 -0500

    Nuevo atributo para pasajero

--- src/ec/edu/utpl/lsquinones/Pasajero.java
+++ src/ec/edu/utpl/lsquinones/Pasajero.java
@@ -14,6 +14,7 @@ public class Pasajero {
     private String nombre;
     private String apellido;
     private int edad;
+    private float peso;
 
     public Pasajero(String nombre) {
         this.nombre = nombre;
