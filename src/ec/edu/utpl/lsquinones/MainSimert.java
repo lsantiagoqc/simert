17849dcbfd7949fffe5615ab93b87d1c3677921a
lsantiago <lsquinones@gmail.com>
2016-07-11 16:57:13 -0500

    Implementación de la clase MainSimert

---
+++ src/ec/edu/utpl/lsquinones/MainSimert.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.utpl.lsquinones;
+
+/**
+ *
+ * @author Santiago Quiñones
+ */
+public class MainSimert {
+    public static void main(String[] args) {
+        Bus miBus = new Bus();
+        
+        System.out.println("Costo de pasaje: " + miBus.getCostoPasaje());
+        
+    }
+}
